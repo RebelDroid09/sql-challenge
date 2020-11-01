9871aa276e89ebd74f183b85246d78f48d717c93
RebelDroid09 <paulcs91@gmail.com>
2020-11-01 13:33:17 -0600

    Update dataAnlysis5.sql
    Formatting changes

--- EmployeeSQL/dataAnlysis5.sql
+++ EmployeeSQL/dataAnlysis5.sql
@@ -1,3 +1,3 @@
 SELECT e.first_name, e.last_name, e.sex
 FROM public."Employees" AS e
-WHERE e.first_name = 'Hercules' and LEFT(e.last_name, 1) = 'B'
+WHERE e.first_name = 'Hercules' and LEFT(e.last_name, 1) = 'B';
