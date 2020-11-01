314124e580764d4b46d42026153ddeb6d5f7bdd0
RebelDroid09 <paulcs91@gmail.com>
2020-11-01 13:33:37 -0600

    Update dataAnlysis7.sql
    Formatting changes

--- EmployeeSQL/dataAnlysis7.sql
+++ EmployeeSQL/dataAnlysis7.sql
@@ -4,4 +4,4 @@ INNER JOIN public."DeptEmp" AS de
 ON e.emp_no = de.emp_no
 INNER JOIN public."Departments" AS d
 ON de.dept_no = d.dept_no
-WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
